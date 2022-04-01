b69ece919dca645b330a1a68d7d8f59a5a02709f
TunaTunado <arthurfurlan13@gmail.com>
2022-03-31 21:30:25 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,8 +6,44 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+
+        Livro livro1 = new Livro("A mão esquerda de Deus", 323);
+        System.out.println(livro1);
          /* int a =5;
         int b =3;
         System.out.println("Hello world! "+ (a+b)); */
+    }
+}
+class Livro {
+    private String name;
+    private Integer numPaginas;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public Livro(String name, Integer numPaginas) {
+        this.name = name;
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "name='" + name + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
